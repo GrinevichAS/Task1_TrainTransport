c21cb28dec74c18e92b751e6b34b7ae3651bb60c
GrinevichAS <Grinevich_AS1@mail.ru>
2017-10-30 12:36:05 +0300

    accessibility error fix

--- TrainTransport/Classes/Carriage.cs
+++ TrainTransport/Classes/Carriage.cs
@@ -3,7 +3,7 @@ using TrainTransport.Interfaces;
 
 namespace TrainTransport.Classes
 {
-    abstract class Carriage : ICarriage
+     abstract class Carriage : ICarriage
     {
         public string Name { get; set; }
         public double Tare { get; set; }
